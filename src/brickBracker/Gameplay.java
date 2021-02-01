edea312d4ff74745d382b0b2d0fa6d4fddf57202
shane1595042264 <sli@cannonschool.org>
2021-02-01 16:31:29 -0500

    Initial commit

---
+++ src/brickBracker/Gameplay.java
@@ -0,0 +1,51 @@
+package brickBracker;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false; //so that when game starts game shouldn't play immediately until we press the button
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer time;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    public Gameplay(){ //Constructor
+
+    }
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
